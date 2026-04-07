d07a1de4b1f7516495a3d188387a1251c2fa89a2
jnimisha <jnimisha@umich.edu>
2026-04-07 16:52:23 -0400

    Part 2 commit
    Added player and dealer plays

--- FinalProjectPart2.py
+++ FinalProjectPart2.py
@@ -18,6 +18,42 @@ def play_game():
     player_choice = ""
     player_bets = 0
 
+    # player's turn to play, they can choose to either stand or hit till they are not bust
+    while not player_hand.is_bust():
+        player_choice = input("Enter H to hit and S to stand :").upper()
+        if player_choice == "H":
+            player_hand.add_card(shoe.deal_card())
+            print("Player Hand :", player_hand.cards)
+        elif player_choice == "S":
+            print("Player chooses to stand")
+            break
+
+    if player_hand.is_bust():
+        print("Player busted, Dealer Wins!")
+        player_bets -= 1
+        return player_hand, dealer_hand, player_choice, player_bets
+
+    # dealer's turn to play, they must hit till they reach a value of at least 17 and then they stand
+    print("Dealer Hand :", dealer_hand.cards)
+    while dealer_hand.get_value() < 17:
+        dealer_hand.add_card(shoe.deal_card())
+        print("Dealer Hand :", dealer_hand.cards)
+
+    if dealer_hand.is_bust():
+        print("Dealer busted, Player Wins!")
+        player_bets += 1
+        return player_hand, dealer_hand, player_choice, player_bets
+
+    # if neither dealer nor player are busted, compare the card values
+    if player_hand.get_value() > dealer_hand.get_value():
+        print("Player Wins!")
+        player_bets += 1
+    elif player_hand.get_value() < dealer_hand.get_value():
+        print("Dealer Wins!")
+        player_bets -= 1
+    else:
+        print("Tie!")
+
     return player_hand, dealer_hand, player_choice, player_bets
 
 
