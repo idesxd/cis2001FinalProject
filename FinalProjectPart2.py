ff57b9d68063a57c9126961d229dfe6ae7bc210a
jnimisha <jnimisha@umich.edu>
2026-04-09 12:45:34 -0400

    update part 2
    updated part 2 to better suit for simulation instead of user input

--- FinalProjectPart2.py
+++ FinalProjectPart2.py
@@ -1,7 +1,10 @@
 from FinalProjectPart1 import *
+import random
+import copy
 
-def play_game():
-    shoe = Shoe()
+def start_game(shoe):
+    if len(shoe.cards) < 100:
+        shoe.build_shoe()
     player_hand = Hand()
     dealer_hand = Hand()
 
@@ -10,6 +13,8 @@ def play_game():
     player_hand.add_card(shoe.deal_card())
     dealer_hand.add_card(shoe.deal_card())
     dealer_hand.add_card(shoe.deal_card())
+    player_hand_org = copy.deepcopy(player_hand)
+    dealer_hand_org = copy.deepcopy(dealer_hand)
 
     # both of the player's cards and first of the dealer's cards is made visible
     print("Player Hand :", player_hand.cards)
@@ -18,20 +23,28 @@ def play_game():
     player_choice = ""
     player_bets = 0
 
-    # player's turn to play, they can choose to either stand or hit till they are not bust
-    while not player_hand.is_bust():
-        player_choice = input("Enter H to hit and S to stand :").upper()
+    # player's turn to play, they can choose to either stand or hit once
+    if not player_hand.is_bust():
+        # player_choice = input("Enter H to hit and S to stand :").upper()
+        player_choice = random.choice(['H', 'S'])
+
         if player_choice == "H":
             player_hand.add_card(shoe.deal_card())
             print("Player Hand :", player_hand.cards)
         elif player_choice == "S":
             print("Player chooses to stand")
-            break
+
+    player_hand, dealer_hand, player_bets, outcome = play_game(shoe, player_hand, dealer_hand, player_bets)
+
+    return player_hand_org, dealer_hand_org, player_choice, player_hand, dealer_hand, player_bets, outcome
+
+def play_game(shoe, player_hand, dealer_hand, player_bets):
 
     if player_hand.is_bust():
         print("Player busted, Dealer Wins!")
         player_bets -= 1
-        return player_hand, dealer_hand, player_choice, player_bets
+        outcome = "Loss"
+        return player_hand, dealer_hand, player_bets, outcome
 
     # dealer's turn to play, they must hit till they reach a value of at least 17 and then they stand
     print("Dealer Hand :", dealer_hand.cards)
@@ -42,19 +55,23 @@ def play_game():
     if dealer_hand.is_bust():
         print("Dealer busted, Player Wins!")
         player_bets += 1
-        return player_hand, dealer_hand, player_choice, player_bets
+        outcome = "Win"
+        return player_hand, dealer_hand, player_bets, outcome
 
     # if neither dealer nor player are busted, compare the card values
     if player_hand.get_value() > dealer_hand.get_value():
         print("Player Wins!")
         player_bets += 1
+        outcome = "Win"
     elif player_hand.get_value() < dealer_hand.get_value():
         print("Dealer Wins!")
         player_bets -= 1
+        outcome = "Loss"
     else:
-        print("Tie!")
+        print("Draw!")
+        outcome = "Draw"
 
-    return player_hand, dealer_hand, player_choice, player_bets
+    return player_hand, dealer_hand, player_bets, outcome
 
 
 
